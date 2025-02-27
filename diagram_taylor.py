4d4fee7dd828249d106d9c0db62fe506cfab21da
Tsuyoshi Wakamatsu <56160789+tsuyoshiwakamatsu@users.noreply.github.com>
2025-02-27 06:39:32 +0100

    Update diagram_taylor.py

--- diagram_taylor.py
+++ diagram_taylor.py
@@ -97,8 +97,10 @@ def sample():
     """Example usage."""
     stdref = 48.491
     stats = [
-        [25.939, 0.385, "ARC NWS"], [29.593, 0.509, "ARC SPG"],
-        [33.125, 0.585, "MED EAST"], [35.807, 0.609, "MED WEST"]
+        [25.939, 0.385, "ARC NWS"], 
+        [29.593, 0.509, "ARC SPG"],
+        [33.125, 0.585, "MED EAST"], 
+        [35.807, 0.609, "MED WEST"]
     ]
 
     fig = plt.figure()
